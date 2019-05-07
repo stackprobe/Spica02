71f26ac3979380a5e03a1adff9ad85fa80360fa8
stackprobe <stackprobes@gmail.com>
2019-05-07 13:02:19 +0900

    coffee break

---
+++ wb2/t20190507/Test0001.java
@@ -0,0 +1,21 @@
+package wb2.t20190507;
+
+import charlotte.tools.StringTools;
+
+public class Test0001 {
+	public static void main(String[] args) {
+		try {
+			test01();
+
+			System.out.println("OK!");
+		}
+		catch(Throwable e) {
+			e.printStackTrace();
+		}
+		System.exit(0);
+	}
+
+	private static void test01() throws Exception {
+		System.out.println(StringTools.MBC_KANA);
+	}
+}
